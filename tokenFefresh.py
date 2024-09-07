105ee20286243f656ed7e1ea7ff4ac36f0fe35b5
VSuvorov <v.suvorov@moedelo.org>
2024-09-07 16:13:15 +0300

    fix except

--- tokenFefresh.py
+++ tokenFefresh.py
@@ -1,4 +1,6 @@
 import json
+import time
+import simplejson
 import requests
 
 def get_Token():
@@ -30,5 +32,25 @@ def get_Token():
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload).json()
-    return response.get("token")
+    while True:
+        try:
+            # Выполняем запрос POST
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            # Проверяем, является ли содержимое ответа корректным JSON
+            if response.text:
+                try:
+                    # Пытаемся декодировать JSON
+                    json_data = response.json()
+                    # Если JSON успешно декодирован, возвращаем токен
+                    return json_data.get("token")
+                except simplejson.errors.JSONDecodeError:
+                    print("Ошибка: получен невалидный JSON. Повтор запроса через 10 секунд.")
+            else:
+                print("Ошибка: получен пустой ответ. Повтор запроса через 10 секунд.")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка запроса: {e}. Повтор запроса через 10 секунд.")
+
+        # Задержка перед следующим запросом
+        time.sleep(10)
